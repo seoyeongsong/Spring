6284fc501945f5d02c343b04290ae6b304b97bb2
seoyeongsong <highhopes0809@gmail.com>
2023-07-09 00:57:24 +0900

    add testcase of spring container for understanding singleton pattern

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,5 +47,24 @@ public class SingletonTest {
 
         // sameAs : == 비교
         // equals :
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void SingletonContainer() {
+//        AppConfig appConfig = new AppConfig();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 호출할 때 마다 같은 인스턴스를 조회하는지 확인해보기
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른지 확인해보기
+        System.out.println("memberService1" + memberService1);
+        System.out.println("memberService2" + memberService2);
+
+        // 서로 다른 객체가 생성되므로 메모리 사용이 비효율적이다.
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
