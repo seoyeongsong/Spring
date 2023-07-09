f5f380b37c49cb775cfb50bf8f9511f254555230
seoyeongsong <highhopes0809@gmail.com>
2023-07-09 23:59:08 +0900

    add new Config for testing ComponentScan

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+// 기존에 생성한 AppConfig에 등록한 스프링 빈을 중복으로 등록하지 않기 위해 예외 조건을 명시해준다.
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -7,6 +11,11 @@ public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
     // constructor를 통해서 어떤 구현객체를 할당하던지 생성해줄 수 있다.
+
+
+    //AppConfig와 달리 @Component 만 스캔하여 MemberServiceImpl을 스프링 빈으로 등록하기 때문에
+    //어떤 구현체를 주입할 지 명시할 수 없다. @Autowired를 사용하여 의존성을 주입해줄 수 있다.
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
