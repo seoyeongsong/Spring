db18542ef1933e472c063356a87722c8ba1985f4
seoyeongsong <highhopes0809@gmail.com>
2023-07-10 22:29:51 +0900

    add test for bean duplication conflict

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -2,19 +2,17 @@ package hello.core.scan;
 
 import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AutoAppConfigTest {
 
     @Test
-    @DisplayName("컴포넌트 스캔과 자동 의존성 주입 확인하기")
+    @DisplayName("수동으로 등록하는 빈과 자동으로 등록되는 빈의 충돌을 확인해보자.")
     void basicScan() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
