075ca58828b1527594f607c3c2e21a03f6c43f3e
seoyeongsong <highhopes0809@gmail.com>
2023-07-24 20:57:21 +0900

    request scope

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,29 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest httpServletRequest) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        String requestUrl = httpServletRequest.getRequestURL().toString();
+        myLogger.setRequestUrl(requestUrl);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
